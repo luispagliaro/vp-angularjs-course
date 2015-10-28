64f34f9e392ad16346277788c20d8ae7855a5f09
Luis Matias Pagliaro <luis.pagliaro@ar-it07963.(none)>
2015-10-28 16:32:19 -0300

    Lab 1 - Ej 4

--- GemStore/app.js
+++ GemStore/app.js
@@ -1,11 +1,21 @@
-var gem = {
+var gems = [{
   name: 'Azurite',
   price: 2.95,
   canPurchase: true,
   soldOut: false
-};
+}, {
+  name: 'Bloodstone',
+  price: 5.95,
+  canPurchase: false,
+  soldOut: false
+}, {
+  name: 'Zircon',
+  price: 3.95,
+  canPurchase: true,
+  soldOut: false
+}];
 
 angular.module('gemStore', [])
   .controller('StoreController', function() {
-  	this.product = gem;
+    this.products = gems;
   });
