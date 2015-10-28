93c5ef23d4034d2f3d47074849ce642196a7c183
Luis Matias Pagliaro <luis.pagliaro@ar-it07963.(none)>
2015-10-28 16:05:13 -0300

    Lab 1 - Ej 2

--- GemStore/app.js
+++ GemStore/app.js
@@ -1,1 +1,9 @@
-angular.module('gemStore', []);
+var gem = {
+  name: 'Azurite',
+  price: 2.95
+};
+
+angular.module('gemStore', [])
+  .controller('StoreController', function() {
+  	this.product = gem;
+  });
