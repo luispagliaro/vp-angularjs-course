b4b7bf8c7bece63c3543d8d4558e62ed428a734f
Luis Matias Pagliaro <luis.pagliaro@ar-it07963.(none)>
2015-10-30 15:59:49 -0300

    Lab 2 - Ej 2

--- GemStore/js/StoreController.js
+++ GemStore/js/StoreController.js
@@ -7,22 +7,25 @@
     name: 'Azurite',
     price: 2.95,
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    images: ['01-Azurite.png', '02-Azurite.png', '03-Azurite.png']
   }, {
     name: 'Bloodstone',
     price: 5.95,
     canPurchase: false,
-    soldOut: false
+    soldOut: false,
+    images: ['01-Bloodstone.png', '02-Bloodstone.png']
   }, {
     name: 'Zircon',
     price: 3.95,
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    images: ['01-Zircon.jpg', '02-Zircon.jpg']
   }];
 
   function Store() {
     var vm = this;
-    
+
     vm.products = gems;
   }
 })();
