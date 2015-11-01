04f8ceab4b44bf37c6ea7576d795c741c193ee72
Luis Pagliaro <Luis Pagliaro>
2015-11-01 16:59:22 -0300

    Lab 2 - Ej 6

--- GemStore/js/StoreController.js
+++ GemStore/js/StoreController.js
@@ -5,22 +5,28 @@
 
   var gems = [{
     name: 'Azurite',
-    price: 2.95,
+    description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.',
+    price: 110.50,
     canPurchase: true,
     soldOut: false,
-    images: ['01-Azurite.png', '02-Azurite.png', '03-Azurite.png']
+    images: ['Azurite_1.gif', 'Azurite_2.gif', 'Azurite_3.gif'],
+    shine: 8
   }, {
     name: 'Bloodstone',
-    price: 5.95,
+    description: 'Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.',
+    price: 22.90,
     canPurchase: false,
     soldOut: false,
-    images: ['01-Bloodstone.png', '02-Bloodstone.png']
+    images: ['Bloodstone_1.gif', 'Bloodstone_2.gif', 'Bloodstone_3.gif'],
+    shine: 9
   }, {
     name: 'Zircon',
-    price: 3.95,
+    description: 'Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.',
+    price: 1100,
     canPurchase: true,
     soldOut: false,
-    images: ['01-Zircon.jpg', '02-Zircon.jpg']
+    images: ['Zircon_1.gif', 'Zircon_2.gif', 'Zircon_3.gif'],
+    shine: 70
   }];
 
   function StoreController() {
--- GemStore/js/TabController.js
+++ GemStore/js/TabController.js
@@ -1,21 +1,21 @@
 (function() {
-angular
-	.module('gemStore')
-	.controller('TabController', TabController);
+  angular
+    .module('gemStore')
+    .controller('TabController', TabController);
 
-	function TabController() {
-		var vm = this;
+  function TabController() {
+    var vm = this;
 
-		vm.tab = 1;
-		vm.isSet = isSet;
-		vm.setTab = setTab;
+    vm.tab = 1;
+    vm.isSet = isSet;
+    vm.setTab = setTab;
 
     function isSet(value) {
-    	return vm.tab == value ? true : false;
+      return vm.tab === value ? true : false;
     };
 
     function setTab(value) {
-    	vm.tab = value;
+      vm.tab = value;
     };
-	}
+  }
 })();
