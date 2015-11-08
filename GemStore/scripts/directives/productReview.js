bf23483d5f21cf697b0ac992ce5c13434eb679a9
Luis Pagliaro <Luis Pagliaro>
2015-11-08 17:45:58 -0300

    Agregado controller de reviews y alias faltantes

---
+++ GemStore/scripts/directives/productReview.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gemStore')
+    .directive('productReview', productReview);
+
+  function productReview() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'views/product-review.html',
+      controller: ReviewController,
+      controllerAs: 'reviewCtrl'
+    };
+
+    return directive;
+
+    function ReviewController() {
+      var vm = this;
+
+      vm.addReview = addReview;
+      vm.resetReview = resetReview;
+
+      function addReview(product) {
+        vm.review.createdOn = Date.now();
+
+        product.reviews.push(vm.review);
+
+        vm.review = {};
+
+        return true;
+      }
+
+      function resetReview() {
+        vm.review = {};
+        vm.review.createdOn = Date.now();
+      }
+
+      resetReview();
+    }
+  }
+})();
